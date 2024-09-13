3f7ff80b890bb88ad99e9c0ea08b0b6e7414eed6
chenxiaoyu <chenxiaoyu@p1.com>
2024-09-13 15:24:29 +0800

    实现通过dataChannel发信令功能。web调用http://localhost:9000，点start看效果

--- cmd/transcribe-server/main.go
+++ cmd/transcribe-server/main.go
@@ -1,65 +1,316 @@
 package main
 
 import (
-	"context"
-	"flag"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
+	"strconv"
+	"time"
 
-	"github.com/rviscarra/webrtc-speech-to-text/internal/rtc"
-	"github.com/rviscarra/webrtc-speech-to-text/internal/session"
-	"github.com/rviscarra/webrtc-speech-to-text/internal/transcribe"
-)
-
-const (
-	httpDefaultPort   = "9000"
-	defaultStunServer = "stun:stun.l.google.com:19302"
+	"github.com/pion/rtp"
+	"github.com/pion/webrtc/v3"
+	"github.com/pion/webrtc/v3/pkg/media/oggreader"
 )
 
 func main() {
+	http.HandleFunc("/session", handleConnections)
 
-	httpPort := flag.String("http.port", httpDefaultPort, "HTTP listen port")
-	stunServer := flag.String("stun.server", defaultStunServer, "STUN server URL (stun:)")
-	speechCred := flag.String("google.cred", "", "Google Speech credentials file")
-	flag.Parse()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "./web/index.html") })
+	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./web"))))
+	log.Println("HTTP server started on :9000")
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
 
-	if *speechCred == "" {
-		log.Fatal("You need to specify the Google credentials file")
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	// 创建p2p连接
+	pcconf := webrtc.Configuration{
+		ICEServers:   []webrtc.ICEServer{{URLs: []string{"stun:stun.l.google.com:19302"}}},
+		SDPSemantics: webrtc.SDPSemanticsUnifiedPlanWithFallback,
 	}
 
-	var tr transcribe.Service
-	ctx := context.Background()
-	tr, err := transcribe.NewGoogleSpeech(ctx, *speechCred)
+	pc, err := webrtc.NewPeerConnection(pcconf)
+	if err != nil {
+		log.Printf("failed:%v \n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
-	var webrtc rtc.Service
-	webrtc = rtc.NewPionRtcService(*stunServer, tr)
-	// webrtc = rtc.NewLoggingService(webrtc)
-
-	// Endpoint to create a new speech to text session
-	http.Handle("/session", session.MakeHandler(webrtc))
-
-	// Serve static assets
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./web/index.html")
+	pc.OnTrack(func(track *webrtc.TrackRemote, r *webrtc.RTPReceiver) {
+		if track.Kind() == webrtc.RTPCodecTypeAudio {
+			log.Printf("Received audio track, id = %s, codec = %v\n", track.ID(), track.Codec())
+			timer := time.NewTimer(5 * time.Second)
+			go func() {
+				for {
+					_, _, err := track.ReadRTP()
+					timer.Reset(1 * time.Second)
+					if err != nil {
+						timer.Stop()
+						log.Printf("ReadRTP failed %v\n", err)
+						return
+					}
+					// log.Printf("receive packet %v\n", packet)
+				}
+			}()
+		}
 	})
-	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./web"))))
 
-	errors := make(chan error, 2)
+	pc.OnICEConnectionStateChange(func(connState webrtc.ICEConnectionState) {
+		log.Printf("OnICEConnectionStateChange state: %s, current time:%s \n", connState.String(), time.Now().Format(time.RFC3339Nano))
+	})
+	pc.OnConnectionStateChange(func(connState webrtc.PeerConnectionState) {
+		log.Printf("OnConnectionStateChange state: %s, current time:%s \n", connState.String(), time.Now().Format(time.RFC3339Nano))
+	})
+	// Register data channel creation handling
+	var dataChannel *DataChannel
+	pc.OnDataChannel(func(d *webrtc.DataChannel) {
+		fmt.Printf("New DataChannel %s %d\n", d.Label(), d.ID())
+		dataChannel = &DataChannel{}
+		dataChannel.webrtcDataChanel = d
+
+		// Register channel opening handling
+		d.OnOpen(func() {
+			fmt.Printf("Data channel '%s'-'%d' on Open\n", d.Label(), d.ID())
+			dataChannel.StartDevDataLoop()
+		})
+		// Register text message handling
+		d.OnMessage(func(msg webrtc.DataChannelMessage) {
+			fmt.Printf("Message from DataChannel '%s': '%s'\n", d.Label(), string(msg.Data))
+		})
+	})
+
+	pc.OnICECandidate(func(candidate *webrtc.ICECandidate) {
+		if candidate == nil {
+			return
+		}
+		candidateJSON, err := json.Marshal(candidate.ToJSON())
+		if err != nil {
+			log.Println("Error marshaling ICE candidate:", err)
+			return
+		}
+		dataChannel.SendData("candidate", string(candidateJSON))
+		log.Printf("OnICECandidate %+v\n", candidate)
+	})
+
+	// pc的某些状态变化回调需要协商，可能给pc添加了一个轨道或者添加了一个datachannel导致的
+	pc.OnNegotiationNeeded(func() {
+		log.Printf("OnNegotiationNeeded\n")
+		offer, err := pc.CreateOffer(nil)
+		if err != nil {
+			log.Printf("error create offer %v\n", err)
+		}
+		err = pc.SetLocalDescription(offer)
+		if err != nil {
+			log.Printf("error set local descriptor%v\n", err)
+		}
+		dataChannel.SendData("offer", offer.SDP)
+	})
+
+	// pc.OnTrack(func(track *webrtc.TrackRemote, receiver *webrtc.RTPReceiver) {
+	// 	log.Printf("OnTrack %v\n", receiver.Track().Codec())
+	// 	playFromDisk("output.ogg", pc)
+	// 	for {
+	// 		// rtp, _, err := track.ReadRTP()
+	// 		_, _, err := track.ReadRTP()
+	// 		if err != nil {
+	// 			log.Println("Error reading RTP packet:", err)
+	// 			return
+	// 		}
+	// 		// fmt.Printf("Received audio RTP packet: %v\n", rtp)
+	// 	}
+	// })
+
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	dec := json.NewDecoder(r.Body)
+	req := newSessionRequest{}
+
+	if err := dec.Decode(&req); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = pc.SetRemoteDescription(webrtc.SessionDescription{
+		SDP:  req.Offer,
+		Type: webrtc.SDPTypeOffer,
+	})
+	if err != nil {
+		log.Printf("SetRemoteDescription failed:%v \n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	answer, err := pc.CreateAnswer(nil)
+	if err != nil {
+		log.Printf("CreateAnswer failed:%v \n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	err = pc.SetLocalDescription(answer)
+	if err != nil {
+		log.Printf("SetLocalDescription failed:%v \n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	payload, err := json.Marshal(newSessionResponse{
+		Answer: answer.SDP,
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	w.Write(payload)
+}
+
+func playFromDisk(audioFileName string, pc *webrtc.PeerConnection) {
+	_, err := os.Stat(audioFileName)
+	haveAudioFile := !os.IsNotExist(err)
+	if !haveAudioFile {
+		log.Printf("No audio file!\n")
+		return
+	}
+	if haveAudioFile {
+		audioTrack, audioTrackErr := webrtc.NewTrackLocalStaticRTP(webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeOpus}, "audio", "pion")
+		if audioTrackErr != nil {
+			panic(audioTrackErr)
+		}
+
+		rtpSender, audioTrackErr := pc.AddTrack(audioTrack)
+		if audioTrackErr != nil {
+			panic(audioTrackErr)
+		}
+
+		// Read incoming RTCP packets
+		// Before these packets are returned they are processed by interceptors. For things
+		// like NACK this needs to be called.
+		go func() {
+			rtcpBuf := make([]byte, 1500)
+			for {
+				if _, _, rtcpErr := rtpSender.Read(rtcpBuf); rtcpErr != nil {
+					return
+				}
+			}
+		}()
+
+		go func() {
+			file, oggErr := os.Open(audioFileName)
+			if oggErr != nil {
+				panic(oggErr)
+			}
+
+			ogg, _, oggErr := oggreader.NewWith(file)
+			if oggErr != nil {
+				panic(oggErr)
+			}
+
+			var lastGranule uint64
+
+			ticker := time.NewTicker(20 * time.Millisecond)
+			defer ticker.Stop()
+
+			sequenceNumber := uint16(0)
+			for ; true; <-ticker.C {
+				pageData, pageHeader, oggErr := ogg.ParseNextPage()
+				if errors.Is(oggErr, io.EOF) {
+					fmt.Printf("All audio pages parsed and sent")
+					break
+				}
+
+				if oggErr != nil {
+					panic(oggErr)
+				}
+
+				sampleCount := float64(pageHeader.GranulePosition - lastGranule)
+				lastGranule = pageHeader.GranulePosition
+				sampleDuration := time.Duration((sampleCount/48000)*1000) * time.Millisecond
+
+				packet := &rtp.Packet{
+					Header: rtp.Header{
+						Version:        2,
+						PayloadType:    111, // Opus payload type
+						SequenceNumber: sequenceNumber,
+						Timestamp:      uint32(lastGranule),
+						SSRC:           12345,
+					},
+					Payload: pageData,
+				}
+				sequenceNumber++
+
+				if oggErr = audioTrack.WriteRTP(packet); oggErr != nil {
+					panic(oggErr)
+				}
+				log.Printf("success WriteRTP %v\n", sampleDuration)
+			}
+		}()
+	}
+}
+
+// ########################### http接口 ###########################
+type newSessionRequest struct {
+	Offer string `json:"offer"`
+}
+type newSessionResponse struct {
+	Answer string `json:"answer"`
+}
+
+// ########################### 对WebRtcDataChannel封装一层 ###########################
+// DataChannelMessage中Data []byte的格式定义举例 (都是json形式)
+// {"key":"offer", "value":"offer的string值"}
+// {"key":"answer", "value":"answer的string值"}
+// {"key":"candidate", "value":"{"sdpMid":"abc", "candidate":"def", "sdpMLineIndex":"123"}"}
+// {"key":"dev", "value":"随意的一个string，用于测试"}
+
+type DataChannel struct {
+	webrtcDataChanel *webrtc.DataChannel
+}
+
+func (c *DataChannel) SendData(key, value string) error { // key: offer, answer, candidate, dev
+	if c == nil {
+		return fmt.Errorf("DataChannel is nil")
+	}
+	m := make(map[string]string)
+	m["key"] = key
+	m["value"] = value
+	v, err := json.Marshal(m)
+	if err != nil {
+		return err
+	}
+	if sendErr := c.webrtcDataChanel.SendText(string(v)); sendErr != nil {
+		return sendErr
+	}
+	return nil
+}
+
+func (c *DataChannel) parseData(message webrtc.DataChannelMessage) (string, string) { // key: offer, answer, candidate, dev
+	var msg map[string]string
+	err := json.Unmarshal(message.Data, &msg)
+	if err != nil {
+		log.Println(err)
+		return "", ""
+	}
+	return msg["key"], msg["value"]
+}
+
+func (c *DataChannel) StartDevDataLoop() {
+	if c == nil {
+		return
+	}
 	go func() {
-		log.Printf("Starting signaling server on port %s", *httpPort)
-		errors <- http.ListenAndServe(fmt.Sprintf(":%s", *httpPort), nil)
+		ticker := time.NewTicker(1 * time.Second)
+		defer ticker.Stop()
+		i := 0
+		for range ticker.C {
+			if sendErr := c.SendData("dev", strconv.Itoa(i)); sendErr != nil {
+				fmt.Printf("SendData get error :%v\n", sendErr)
+			} else {
+				fmt.Printf("SendData :%v, DataChannelId:%v\n", i, c.webrtcDataChanel.ID())
+			}
+			i++
+		}
 	}()
-
-	go func() {
-		interrupt := make(chan os.Signal)
-		signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
-		errors <- fmt.Errorf("Received %v signal", <-interrupt)
-	}()
-
-	err = <-errors
-	log.Printf("%s, exiting.", err)
 }
--- go.mod
+++ go.mod
@@ -1,31 +1,81 @@
 module github.com/rviscarra/webrtc-speech-to-text
 
-go 1.12
+go 1.21
+
+toolchain go1.22.7
 
 require (
-	cloud.google.com/go v0.40.0
-	github.com/golang/mock v1.3.1 // indirect
-	github.com/google/btree v1.0.0 // indirect
-	github.com/google/pprof v0.0.0-20190515194954-54271f7e092f // indirect
-	github.com/googleapis/gax-go/v2 v2.0.5 // indirect
-	github.com/gorilla/websocket v1.4.0
-	github.com/kr/pty v1.1.5 // indirect
-	github.com/pion/webrtc/v2 v2.0.15
-	github.com/satori/go.uuid v1.2.0
-	go.opencensus.io v0.22.0 // indirect
-	golang.org/x/crypto v0.0.0-20190611184440-5c40567a22f8 // indirect
-	golang.org/x/exp v0.0.0-20190510132918-efd6b22b2522 // indirect
-	golang.org/x/image v0.0.0-20190523035834-f03afa92d3ff // indirect
-	golang.org/x/mobile v0.0.0-20190607214518-6fa95d984e88 // indirect
-	golang.org/x/mod v0.1.0 // indirect
-	golang.org/x/net v0.0.0-20190613194153-d28f0bde5980 // indirect
-	golang.org/x/sys v0.0.0-20190614160838-b47fdc937951 // indirect
-	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
-	golang.org/x/tools v0.0.0-20190614205625-5aca471b1d59 // indirect
-	google.golang.org/api v0.6.0
-	google.golang.org/appengine v1.6.1 // indirect
-	google.golang.org/genproto v0.0.0-20190611190212-a7e196e89fd3
-	google.golang.org/grpc v1.21.1 // indirect
+	cloud.google.com/go/speech v1.25.1
+	github.com/pion/rtp v1.8.7
+	github.com/pion/webrtc/v2 v2.2.26
+	github.com/pion/webrtc/v3 v3.3.1
+	google.golang.org/api v0.197.0
+	google.golang.org/genproto v0.0.0-20240903143218-8af14fe29dc1
 	gopkg.in/hraban/opus.v2 v2.0.0-20180426093920-0f2e0b4fc6cd
-	honnef.co/go/tools v0.0.0-20190614002413-cb51c254f01b // indirect
+)
+
+require (
+	cloud.google.com/go v0.115.1 // indirect
+	cloud.google.com/go/auth v0.9.3 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.4 // indirect
+	cloud.google.com/go/compute/metadata v0.5.0 // indirect
+	cloud.google.com/go/longrunning v0.6.0 // indirect
+	github.com/cheekybits/genny v1.0.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/google/s2a-go v0.1.8 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
+	github.com/googleapis/gax-go/v2 v2.13.0 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/lucas-clemente/quic-go v0.7.1-0.20190401152353-907071221cf9 // indirect
+	github.com/marten-seemann/qtls v0.2.3 // indirect
+	github.com/nxadm/tail v1.4.8 // indirect
+	github.com/pion/datachannel v1.5.8 // indirect
+	github.com/pion/dtls/v2 v2.2.12 // indirect
+	github.com/pion/ice v0.7.18 // indirect
+	github.com/pion/ice/v2 v2.3.34 // indirect
+	github.com/pion/interceptor v0.1.29 // indirect
+	github.com/pion/logging v0.2.2 // indirect
+	github.com/pion/mdns v0.0.12 // indirect
+	github.com/pion/quic v0.1.1 // indirect
+	github.com/pion/randutil v0.1.0 // indirect
+	github.com/pion/rtcp v1.2.14 // indirect
+	github.com/pion/sctp v1.8.19 // indirect
+	github.com/pion/sdp/v2 v2.4.0 // indirect
+	github.com/pion/sdp/v3 v3.0.9 // indirect
+	github.com/pion/srtp v1.5.1 // indirect
+	github.com/pion/srtp/v2 v2.0.20 // indirect
+	github.com/pion/stun v0.6.1 // indirect
+	github.com/pion/transport v0.10.1 // indirect
+	github.com/pion/transport/v2 v2.2.10 // indirect
+	github.com/pion/turn/v2 v2.1.6 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/wlynxg/anet v0.0.3 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.54.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.54.0 // indirect
+	go.opentelemetry.io/otel v1.29.0 // indirect
+	go.opentelemetry.io/otel/metric v1.29.0 // indirect
+	go.opentelemetry.io/otel/trace v1.29.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/oauth2 v0.23.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	golang.org/x/time v0.6.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240903143218-8af14fe29dc1 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
+	google.golang.org/grpc v1.66.1 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
